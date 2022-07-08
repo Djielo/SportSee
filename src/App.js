440a9e8d8c9e77f016ebe819e625e857f4683fb0
Djielo <business.jlo68@gmail.com>
2022-07-08 18:59:25 +0200

    Erase Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
 import Error from "./pages/Error/Error";
 
 const App = () => {
@@ -13,7 +12,6 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/*" element={<Error />} />
       </Routes>
-      <Footer />
     </React.Fragment>
   );
 };
