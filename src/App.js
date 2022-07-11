2bd109aa597f60f6140ee2f2cbf7216a0598f325
Djielo <business.jlo68@gmail.com>
2022-07-11 18:35:34 +0200

    DataCards  +BG on graph1 and graph2 + 1stName

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Error from "./pages/Error/Error";
-
+import { DataContextProvider } from "./Context/DataContext"
 const App = () => {
   return (
     <React.Fragment>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
+      <DataContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </DataContextProvider>
     </React.Fragment>
   );
 };
--- src/components/DatasCards/DatasCards.jsx
+++ src/components/DatasCards/DatasCards.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import DataGenericCard from "../../components/DataGenericCard/DataGenericCard";
 import CaloriesIcon from "../../assets/icon_calories.svg";
 import ProteinIcon from "../../assets/icon_protein.svg";
 import GlucidIcon from "../../assets/icon_glucid.svg";
 import LipidIcon from "../../assets/icon_lipid.svg";
-
+import { DataContext } from "../../Context/DataContext";
 
 const DataCards = () => {
+  const [data] = useContext(DataContext);
+  console.log(data);
+
+  if (data.keyData === undefined) return (<p>Loading</p>)
   return (
     <div className="datas">
-      <DataGenericCard icon={CaloriesIcon} data={`1920kCal`} category={"Calories"} />
-      <DataGenericCard icon={ProteinIcon} data={`155g`} category={"Proteines"} />
-      <DataGenericCard icon={GlucidIcon} data={`290g`} category={"Glucides"} />
-      <DataGenericCard icon={LipidIcon} data={`50g`} category={"Lipides"} />
+      <DataGenericCard icon={CaloriesIcon} data={`${data.keyData.calorieCount}Kcal`} category={"Calories"} />
+      <DataGenericCard icon={ProteinIcon} data={`${data.keyData.proteinCount}g`} category={"Proteines"} />
+      <DataGenericCard icon={GlucidIcon} data={`${data.keyData.carbohydrateCount}g`} category={"Glucides"} />
+      <DataGenericCard icon={LipidIcon} data={`${data.keyData.lipidCount}g`} category={"Lipides"} />
     </div>
   );
 };
